980bdd520421770b039226b12bcf0d000c016e1f
adkau <adkaufman@live.com>
2026-04-26 18:48:40 -0700

    learning to use git

--- SudokuBoard.java
+++ SudokuBoard.java
@@ -1,6 +1,9 @@
-#
+// Aaron Kaufman
+// CS 143
+// Creating a Sudoku Board
 package Sudoku;
 
 public class SudokuBoard {
-    
+    //2d array as the sudoku board, 1-3 first collumn, 4-6 second collum, 7-9 third collumn for layout
+    int[][] board = new int[9][9];
 }
